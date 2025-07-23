f20fca1b3c299a48e819478012e419b91ef891c8
abedb961 <abedbaydoun961@hotmail.com>
2025-07-23 15:25:40 +0300

    added app for route rendering

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import AppRoutes from './routes';
+import Navbar from './components/Navbar';
+import { useLocation } from 'react-router-dom';
+
+export default function App() {
+  const location = useLocation();
+  const hideNavbar = ["/login", "/register", "/forgot-password"].includes(location.pathname);
+
+  return (
+    <>
+      {!hideNavbar && <Navbar />}
+      <AppRoutes />
+    </>
+  );
+}
